474393c7779d88afbc3985c4913e7b50711b47f8
Yasir KILINÇ <63153220+exTypen@users.noreply.github.com>
2021-01-23 16:46:33 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T_k, T_v>
+    {
+        T_k[] keys;
+        T_v[] values;
+
+        public MyDictionary()
+        {
+            keys = new T_k[0];
+            values = new T_v[0];
+        }
+
+        public void Add(T_k key, T_v value)
+        {
+            T_k[] tempKeys = keys;
+            T_v[] tempValues = values;
+
+            keys = new T_k[keys.Length + 1];
+            values = new T_v[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+            }
+
+            keys[tempKeys.Length] = key;
+            values[tempValues.Length] = value;
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine("\n" + "Key : " + keys[i]);
+                Console.WriteLine("Value : " + values[i] + "\n");
+            }
+        }
+        public void Clear()
+        {
+            keys = new T_k[0];
+            values = new T_v[0];
+            Console.WriteLine("--------------------------------");
+        }
+
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> notlar = new MyDictionary<string, int>();
+            notlar.Add("Ahmet", 90);
+            notlar.Add("Mehmet", 80);
+            notlar.Add("Yasir", 100);
+            notlar.Add("Furkan", 70);
+            notlar.Add("Yusuf", 40);
+
+            notlar.Print();
+
+            notlar.Clear();
+
+            notlar.Add("DENEME", 10);
+
+            notlar.Print();
+        }
+    }
+}
